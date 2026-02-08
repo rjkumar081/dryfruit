c90f7d4e1e579650a9ac54b18e4d11a0dfe47656
rjkumar081 <97723829+rjkumar081@users.noreply.github.com>
2026-02-08 11:50:45 +0530

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,34 +1,7 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dot");
-
-function showSlide(n) {
-  slides.forEach(slide => slide.style.display = "none");
-  dots.forEach(dot => dot.classList.remove("active"));
-
-  slideIndex = (n + slides.length) % slides.length;
-
-  slides[slideIndex].style.display = "block";
-  dots[slideIndex].classList.add("active");
-}
-
-/* Arrows */
-document.querySelector(".prev").addEventListener("click", () => {
-  showSlide(slideIndex - 1);
-});
-document.querySelector(".next").addEventListener("click", () => {
-  showSlide(slideIndex + 1);
-});
-
-/* Dots */
-dots.forEach((dot, i) => {
-  dot.addEventListener("click", () => showSlide(i));
-});
-
-/* Auto play */
-setInterval(() => {
-  showSlide(slideIndex + 1);
-}, 5000);
-
-/* Init */
-showSlide(0);
+let index=0;
+const slides=document.querySelectorAll(".slide");
+setInterval(()=>{
+slides.forEach(s=>s.classList.remove("active"));
+index=(index+1)%slides.length;
+slides[index].classList.add("active");
+},4000);
