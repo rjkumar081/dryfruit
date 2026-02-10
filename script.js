a3b10341fd900cfea90b4e73a13b45c1922a5670
rjkumar081 <97723829+rjkumar081@users.noreply.github.com>
2026-02-10 17:06:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-// Future: form validation / analytics
-console.log("Royal Dry Fruits Loaded");
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dot");
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  dots.forEach(dot => dot.classList.remove("active"));
+
+  slides[i].classList.add("active");
+  dots[i].classList.add("active");
+}
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    index = i;
+    showSlide(index);
+  });
+});
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 5000);
